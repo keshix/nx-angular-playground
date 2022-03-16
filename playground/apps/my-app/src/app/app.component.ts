09590c01904042d06dec97a3c2628293cfce3455
Jan Uhlmann <jan.uhlmann@dser.de>
2022-03-16 11:36:32 +0100

    add bootstrap 5

--- playground/apps/my-app/src/app/app.component.ts
+++ playground/apps/my-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'playground-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less'],
+    selector: 'playground-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-app';
+    title = 'my-app';
 }
