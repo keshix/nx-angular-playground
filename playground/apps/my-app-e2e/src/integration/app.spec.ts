4e2abcdabbf2d8407fb67a1916b8d97742ada6fa
Jan Uhlmann <jan.uhlmann@dser.de>
2022-03-16 13:53:07 +0100

    update e2e tests

--- playground/apps/my-app-e2e/src/integration/app.spec.ts
+++ playground/apps/my-app-e2e/src/integration/app.spec.ts
@@ -1,13 +1,12 @@
+/// <reference types="cypress"/>
 import { getGreeting } from '../support/app.po';
 
 describe('my-app', () => {
-  beforeEach(() => cy.visit('/'));
+    beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    it('should display welcome message', () => {
+        cy.login('my-email@something.com', 'myPassword');
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome my-app');
-  });
+        getGreeting().contains('Welcome');
+    });
 });
