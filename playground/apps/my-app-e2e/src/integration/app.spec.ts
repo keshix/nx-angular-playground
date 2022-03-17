a67a62fede25912ccfbf6b1efa47a2124ea766b5
Jan Uhlmann <jan.uhlmann@dser.de>
2022-03-17 15:50:50 +0100

    feat: e2e integration tests

--- playground/apps/my-app-e2e/src/integration/app.spec.ts
+++ playground/apps/my-app-e2e/src/integration/app.spec.ts
@@ -1,12 +1,39 @@
 /// <reference types="cypress"/>
-import { getGreeting } from '../support/app.po';
+import {
+    getGreeting,
+    getQuestionDetailsFirstLink,
+    getQuestionDetailsPageHeading,
+    getQuestionForm,
+    getQuestionTitleInput,
+    getQuestionTitleInputErrorMessage,
+} from '../support/app.po';
 
 describe('my-app', () => {
     beforeEach(() => cy.visit('/'));
 
     it('should display welcome message', () => {
-        cy.login('my-email@something.com', 'myPassword');
+        getGreeting().contains('Welcome to Staffbase');
+    });
 
-        getGreeting().contains('Welcome');
+    it('should present a form to create questions', () => {
+        getQuestionForm().should('be.visible');
+    });
+
+    it('should validate inputs', () => {
+        getQuestionTitleInput()
+            .type('foo')
+            .clear()
+            .then(() => {
+                getQuestionTitleInputErrorMessage().should('be.visible');
+                getQuestionTitleInput().type('foo');
+            });
+    });
+
+    it('should show question details', () => {
+        getQuestionDetailsFirstLink()
+            .click()
+            .then(() => {
+                getQuestionDetailsPageHeading().should('be.visible');
+            });
     });
 });
--- playground/apps/my-app-e2e/src/support/app.po.ts
+++ playground/apps/my-app-e2e/src/support/app.po.ts
@@ -1,1 +1,6 @@
 export const getGreeting = () => cy.get('h1');
+export const getQuestionForm = () => cy.get('[data-cy="question-form"]');
+export const getQuestionTitleInput = () => cy.get('[data-cy="question-title-input"]');
+export const getQuestionTitleInputErrorMessage = () => cy.get('[data-cy="question-title-input-error-message"]');
+export const getQuestionDetailsFirstLink = () => cy.get('[data-cy="question-details-link"]').first();
+export const getQuestionDetailsPageHeading = () => cy.get('[data-cy="question-details-heading"]');
